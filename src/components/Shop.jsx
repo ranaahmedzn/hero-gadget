ca9f665450a2fd9dd18d32cbc878998ce99e2e31
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-09 17:16:41 +0600

    update add to cart button funtionality

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,31 @@
 import React, { useContext } from 'react';
 import ProductCard from './Cards/ProductCard';
-import { addToDb } from '../utilities/fakeDb';
-import { ProductsContext } from '../App';
+import { addToDb} from '../utilities/fakeDb';
+import { CartContext, ProductsContext } from '../App';
+import { toast } from 'react-hot-toast';
 
 const Shop = () => {
     const products = useContext(ProductsContext)
+    const [cart, setCart] = useContext(CartContext)
 
-    const handleAddToCart = (id) => {
-        addToDb(id)
+    const handleAddToCart = (product) => {
+        let newCart = []
+
+        const exists = cart.find(pd => pd.id === product.id)
+
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const rest = cart.filter(pd => pd.id !== exists.id)
+            newCart = [...rest, exists]
+        }
+
+        addToDb(product.id)
+        setCart(newCart)
+        toast.success("Product Added!🛒")
     }
 
     return (
