e71ba3d094e07a9c499e3b07c5250f0a95b6868d
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-09 14:36:01 +0600

    initial react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <h1>Welcome to HeroGadget</h1>
-}
+import React from 'react';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
 
-export default App
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <div className='min-h-[calc(100vh-157px)]'>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: 'shop',
+                element: <h2>This is shop component</h2>
+            },
+            {
+                path: 'cart',
+                element: <h2>This is cart component</h2>
+            },
+            {
+                path: 'about',
+                element: <About />
+            }
+        ]
+    }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
